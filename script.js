c25ed6bff5a04420863c7388051aeedb45f593fb
paccvvet <120043593+paccvvet@users.noreply.github.com>
2024-09-27 20:40:17 +0300

    create element for cookie

--- script.js
+++ script.js
@@ -31,3 +31,38 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+// Выбор элементов
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.querySelector('.header'));
+// const sections = document.querySelectorAll('.section');
+// console.log(sections);
+// console.log(document.getElementById('section--1'));
+// const buttons = document.getElementsByTagName('button');
+// console.log(buttons);
+// console.log(document.getElementsByClassName('btn'));
+
+// создание и вставка элементов
+//.insertAjecentHTML()
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = "Мы используем cookie.";
+message.innerHTML =
+  'Мы используем cookie. <button class="btn btn-close-cookie">Ok</button>';
+const header = document.querySelector('.header');
+header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Удаление
+
+document
+  .querySelector('.btn-close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
