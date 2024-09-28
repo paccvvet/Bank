27e0e5e9389ff1953d2da701b34f03ff0792ab9f
paccvvet <120043593+paccvvet@users.noreply.github.com>
2024-09-28 17:00:10 +0300

    add smooth scrolling

--- script.js
+++ script.js
@@ -66,3 +66,67 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// // Стили
+// message.style.backgroundColor = '#076785';
+// message.style.width = '120%';
+// console.log(message.style.width);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+// console.log();
+// console.log(getComputedStyle(message).height);
+
+// // Атрибуты
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+// logo.alt = 'Лого банка';
+
+// logo.setAttribute('copyrigth', 'Рассвет');
+
+// const link = document.querySelector('.nav__link--btn');
+
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// // data artibutes
+// console.log(logo.dataset.versionNumber);
+
+// // classes
+
+// logo.classList.add('a', 'b');
+// logo.classList.remove('a', 'b');
+// logo.classList.toggle('a');
+// logo.classList.contains('c');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const section1Coords = section1.getBoundingClientRect();
+  console.log(section1Coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('tekyshee prokryt: x, y', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'Ширина и высота viewport',
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+
+  // window.scrollTo(
+  //   section1Coords.left + window.pageXOffset,
+  //   section1Coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  // Only for newBrosers
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
