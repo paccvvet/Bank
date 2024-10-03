8976225a15c5665c6d3d12bc2b5a3158c2d2cd61
paccvvet <120043593+paccvvet@users.noreply.github.com>
2024-10-03 13:05:05 +0300

    feat: tab opening

--- script.js
+++ script.js
@@ -55,6 +55,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Вкладки
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContents = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', function (e) {
+  const clickedButton = e.target.closest('.operations__tab');
+  //Guard clause
+  if (!clickedButton) return;
+  // Активная вкладка
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clickedButton.classList.add('operations__tab--active');
+  // Активный контент
+  tabContents.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clickedButton.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 ////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
